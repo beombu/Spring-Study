cf8392b50545a76f04cd258143f61fffa19bcfd8
beombu <sunil1369@bible.ac.kr>
2023-01-13 16:36:10 +0900

    Feat(6-76) : Spring data JPA 기본 문법

--- learn-spring-framework/learn-jpa-and-hibernate/src/main/java/com/BeomChul/springBoot/learnjpaandhibernate/course/CourseJdbcCommandLineRunner.java
+++ learn-spring-framework/learn-jpa-and-hibernate/src/main/java/com/BeomChul/springBoot/learnjpaandhibernate/course/CourseJdbcCommandLineRunner.java
@@ -1,8 +1,6 @@
 package com.BeomChul.springBoot.learnjpaandhibernate.course;
 
-import com.BeomChul.springBoot.learnjpaandhibernate.course.Course;
-import com.BeomChul.springBoot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
-import com.BeomChul.springBoot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.BeomChul.springBoot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,18 +14,30 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS JPA!", "BeomChul"));
-        repository.insert(new Course(2, "Learn Azure JPA!", "BeomChul"));
-        repository.insert(new Course(3, "Learn DevOps JPA!", "BeomChul"));
+        repository.save(new Course(1, "Learn AWS JPA!", "BeomChul"));
+        repository.save(new Course(2, "Learn Azure JPA!", "BeomChul"));
+        repository.save(new Course(3, "Learn DevOps JPA!", "BeomChul"));
 
         repository.deleteById(1L);
 
         System.out.println(repository.findById(2L));
         System.out.println(repository.findById(3L));
+
+        System.out.println(repository.count());
+        System.out.println(repository.findAll());
+
+        System.out.println(repository.findByAuthor("BeomChul"));
+        System.out.println(repository.findByAuthor(""));
+
+        System.out.println(repository.findByName("Learn AWS JPA!"));
+        System.out.println(repository.findByName("Learn DevOps JPA!"));
     }
 }
