def33a528c40cab0f9cf2c8db9292040545af6de
beombu <sunil1369@bible.ac.kr>
2023-01-19 19:38:58 +0900

    Feat(7-106) : todolist 삭제, 수정 기능 - 파라미터를 통해 객체 식별

--- learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/TodoController.java
+++ learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/TodoController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.validation.Valid;
@@ -42,6 +43,30 @@ public class TodoController {
         }
 
         todoService.addTodo((String) model.getAttribute("name"), todo.getDescription(), LocalDate.now(), false);
+
+        return "redirect:list-todos";
+    }
+
+    @RequestMapping(value = "delete-todo")
+    public String deleteTodo(@RequestParam int id) {
+        todoService.deleteById(id);
+        return "redirect:list-todos";
+    }
+
+    @RequestMapping(value = "update-todo", method = RequestMethod.GET)
+    public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+        Todo todo = todoService.findById(id);
+        model.addAttribute("todo", todo);
+        return "todo";
+    }
+
+    @RequestMapping(value = "update-todo", method = RequestMethod.POST)
+    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "todo";
+        }
+        todo.setUsername((String) model.getAttribute("name"));
+        todoService.updateTodo(todo);
 
         return "redirect:list-todos";
     }
--- learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/TodoService.java
+++ learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/TodoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class TodoService {
@@ -26,6 +27,23 @@ public class TodoService {
 
     public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
         Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+        todos.add(todo);
+    }
+
+    public void deleteById(int id) {
+        //predicate에는 조건이 들어감
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        //predicate에는 조건이 들어감
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        return todos.stream().filter(predicate).findFirst().get();
+    }
+
+    public void updateTodo(Todo todo) {
+        deleteById(todo.getId());
         todos.add(todo);
     }
 }
