0aa0f8547ceeb29f421dba98aac83230f9f4b0e0
beombu <sunil1369@bible.ac.kr>
2023-01-20 19:34:49 +0900

    Feat(7-118) : SpringSecurity FilterChain(request 요청에 대한 필터 환경 설정) & h2 db 연결 & Todo 객체 @Entity로 등록

--- learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/Todo.java
+++ learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/Todo.java
@@ -2,15 +2,24 @@ package com.beomchul.springboot.mywebapp.todo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue
     private int id;
     private String username;
 
---
+++ learn-spring-framework/mywebapp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'beomchul', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'beomchul', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'beomchul', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'beomchul', 'Learn DevOps', CURRENT_DATE(), false);
