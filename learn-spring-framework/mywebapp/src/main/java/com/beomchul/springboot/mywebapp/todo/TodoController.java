88116b64b0df5503daa24ef626ef059783f95423
beombu <sunil1369@bible.ac.kr>
2023-01-20 18:49:06 +0900

    Feat(7-115) : spring security를 통해 passwordEncoding, UserDetail 생성 학습

--- learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/TodoController.java
+++ learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/TodoController.java
@@ -1,5 +1,7 @@
 package com.beomchul.springboot.mywebapp.todo;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -23,15 +25,22 @@ public class TodoController {
 
     @RequestMapping("list-todos")
     public String listAllTodos(ModelMap model) {
-        List<Todo> todos = todoService.findByUsername("beomchul");
+        String username = getLoggedInUsername(model);
+        List<Todo> todos = todoService.findByUsername(username);
         model.addAttribute("todos", todos);
 
         return "listTodos";
     }
 
+    private String getLoggedInUsername(ModelMap model) {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+
     @RequestMapping(value = "add-todo", method = RequestMethod.GET)
     public String showNewTodoPage(ModelMap model) {
-        Todo todo = new Todo(0, (String) model.getAttribute("name"), "", LocalDate.now().plusYears(1), false);
+        Todo todo = new Todo(0, getLoggedInUsername(model), "", LocalDate.now().plusYears(1), false);
         model.put("todo", todo);
         return "todo";
     }
@@ -42,7 +51,7 @@ public class TodoController {
             return "todo";
         }
 
-        todoService.addTodo((String) model.getAttribute("name"), todo.getDescription(), todo.getTargetDate(), false);
+        todoService.addTodo(getLoggedInUsername(model), todo.getDescription(), todo.getTargetDate(), false);
 
         return "redirect:list-todos";
     }
@@ -65,7 +74,7 @@ public class TodoController {
         if (result.hasErrors()) {
             return "todo";
         }
-        todo.setUsername((String) model.getAttribute("name"));
+        todo.setUsername(getLoggedInUsername(model));
         todoService.updateTodo(todo);
 
         return "redirect:list-todos";
--- learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/TodoService.java
+++ learn-spring-framework/mywebapp/src/main/java/com/beomchul/springboot/mywebapp/todo/TodoService.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
@@ -22,7 +23,9 @@ public class TodoService {
     }
 
     public List<Todo> findByUsername(String username) {
-        return todos;
+        Predicate<? super Todo> predicate =
+                todo -> todo.getUsername().equalsIgnoreCase(username);
+        return todos.stream().filter(predicate).collect(Collectors.toList());
     }
 
     public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
