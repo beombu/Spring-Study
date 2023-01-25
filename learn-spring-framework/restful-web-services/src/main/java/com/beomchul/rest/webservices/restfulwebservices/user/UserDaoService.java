51d1ab96952ac5cb61d437746ac3acfe6c8764a9
beombu <sunil1369@bible.ac.kr>
2023-01-25 14:32:33 +0900

    Feat(8-139) : URI 생성 &&  잘못된 id값 입력시 에러 코드 반환

--- learn-spring-framework/restful-web-services/src/main/java/com/beomchul/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ learn-spring-framework/restful-web-services/src/main/java/com/beomchul/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -11,18 +11,26 @@ import java.util.function.Predicate;
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
 
+    private static int usersCount = 0;
+
     static {
-        users.add(new User(1, "beom", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "chul", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Eve", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount, "beom", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "chul", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "Eve", LocalDate.now().minusYears(20)));
     }
 
     public List<User> findAll() {
         return users;
     }
 
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
-        return users.stream().filter(predicate).findFirst().get();
+        return users.stream().filter(predicate).findFirst().orElse(null);
     }
 }
--- learn-spring-framework/restful-web-services/src/main/java/com/beomchul/rest/webservices/restfulwebservices/user/UserResource.java
+++ learn-spring-framework/restful-web-services/src/main/java/com/beomchul/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.beomchul.rest.webservices.restfulwebservices.user;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,11 +20,23 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException("id : " + id);
+        }
+
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
 
+        return ResponseEntity.created(location).build();
     }
 }
