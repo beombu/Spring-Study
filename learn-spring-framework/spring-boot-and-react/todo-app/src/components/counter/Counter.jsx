d7ffcb758fbd506da954d0d5d4f3ba421b9b6aa9
beombu <sunil1369@bible.ac.kr>
2023-02-02 20:22:30 +0900

    Feat(10-199) : React Hooks & Function Code

---
+++ learn-spring-framework/spring-boot-and-react/todo-app/src/components/counter/Counter.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './Counter.css';
+import CounterButton from './CounterButton';
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    function incrementCounterFunction(by) {
+        setCount(count + by);
+    }
+
+    function decrementCounterFunction(by) {
+        setCount(count - by);
+    }
+
+    function resetCounter() {
+        setCount(0);
+    }
+
+    return (
+        <>
+            <span className="totalCount">{count}</span>
+            <CounterButton by={1} incrementMethod={incrementCounterFunction} decrementMethod={decrementCounterFunction} />
+            <CounterButton by={2} incrementMethod={incrementCounterFunction} decrementMethod={decrementCounterFunction} />
+            <CounterButton by={5} incrementMethod={incrementCounterFunction} decrementMethod={decrementCounterFunction} />
+            <button className="resetButton" onClick={resetCounter}>
+                Reset
+            </button>
+        </>
+    );
+}
