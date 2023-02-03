9cbd9d1d50b3ae8ead94b72c85148a4bda079ddc
beombu <sunil1369@bible.ac.kr>
2023-02-03 19:10:54 +0900

    Feat(11-213) : Header, Footer & Bootstrap 적용

--- learn-spring-framework/spring-boot-and-react/todo-app/src/components/todo/TodoApp.jsx
+++ learn-spring-framework/spring-boot-and-react/todo-app/src/components/todo/TodoApp.jsx
@@ -6,13 +6,16 @@ export default function TodoApp() {
     return (
         <div className="TodoApp">
             <BrowserRouter>
+                <HeaderComponent />
                 <Routes>
                     <Route path="/" element={<LoginComponent />} />
                     <Route path="/login" element={<LoginComponent />} />
                     <Route path="/welcome/:username" element={<WelcomeComponent />} />
                     <Route path="/todos" element={<ListTodosComponent />} />
+                    <Route path="/logout" element={<LogoutComponent />} />
                     <Route path="*" element={<ErrorComponent />} />
                 </Routes>
+                <FooterComponent />
             </BrowserRouter>
         </div>
     );
@@ -52,10 +55,10 @@ function ListTodosComponent() {
     ];
 
     return (
-        <div className="ListTodosComponent">
+        <div className="container">
             <h1>Things You Want To Do!</h1>
             <div>
-                <table>
+                <table className="table">
                     <thead>
                         <tr>
                             <td>ID</td>
@@ -76,6 +79,65 @@ function ListTodosComponent() {
                     </tbody>
                 </table>
             </div>
+        </div>
+    );
+}
+
+function HeaderComponent() {
+    return (
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+            <div className="container">
+                <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.in28minutes.com">
+                            in28minutes
+                        </a>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5">
+                                    <Link className="nav-link" to="/welcome/in28minutes">
+                                        Home
+                                    </Link>
+                                </li>
+                                <li className="nav-item fs-5">
+                                    <Link className="nav-link" to="/todos">
+                                        Todos
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5">
+                                <Link className="nav-link" to="/login">
+                                    Login
+                                </Link>
+                            </li>
+                            <li className="nav-item fs-5">
+                                <Link className="nav-link" to="/logout">
+                                    Logout
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+function FooterComponent() {
+    return (
+        <footer className="footer">
+            <div className="container">Your Footer</div>
+        </footer>
+    );
+}
+
+function LogoutComponent() {
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>Thank you for using our App. Come back soon!</div>
         </div>
     );
 }
