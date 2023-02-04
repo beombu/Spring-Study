289f4fcd79fbd5869f4f42990bf4a03d8f7eaff8
beombu <sunil1369@bible.ac.kr>
2023-02-04 23:23:56 +0900

    Style(11-214) : 모듈 분리

---
+++ learn-spring-framework/spring-boot-and-react/todo-app/src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,10 @@
+function ErrorComponent() {
+    return (
+        <div className="ErrorComponent">
+            <h1>에러!</h1>
+            <div>404 Error!!</div>
+        </div>
+    );
+}
+
+export default ErrorComponent;
---
+++ learn-spring-framework/spring-boot-and-react/todo-app/src/components/todo/LoginComponent.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function LoginComponent() {
+    const [username, setUsername] = useState('beom');
+    const [password, setPassword] = useState('');
+    const [showSeccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+    const navigate = useNavigate();
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
+    function handleSubmit() {
+        if (username === 'beom' && password === '123') {
+            console.log('success');
+            setShowSuccessMessage(true);
+            setShowErrorMessage(false);
+            navigate(`/welcome/${username}`);
+        } else {
+            console.log('failed');
+            setShowSuccessMessage(false);
+            setShowErrorMessage(true);
+        }
+    }
+
+    function SuccessMessageComponent() {
+        if (showSeccessMessage) {
+            return <div className="successMessage">로그인 성공</div>;
+        }
+
+        return null;
+    }
+
+    function ErrorMessageComponent() {
+        if (showErrorMessage) {
+            return <div className="errorMessage">로그인 실패</div>;
+        }
+
+        return null;
+    }
+
+    return (
+        <div className="Login">
+            <SuccessMessageComponent />
+            <ErrorMessageComponent />
+            <div className="LoginForm">
+                <div>
+                    <label>User Name</label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange} />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+                </div>
+                <div>
+                    <button type="button" name="login" onClick={handleSubmit}>
+                        Login
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginComponent;
