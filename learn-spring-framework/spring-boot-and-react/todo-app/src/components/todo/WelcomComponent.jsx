21cf8b879467c0aea6dfa2f3c3262de539dca9ac
beombu <sunil1369@bible.ac.kr>
2023-02-05 18:26:53 +0900

    Add(12-223) : Axios 통신 추가

--- learn-spring-framework/spring-boot-and-react/todo-app/src/components/todo/WelcomComponent.jsx
+++ learn-spring-framework/spring-boot-and-react/todo-app/src/components/todo/WelcomComponent.jsx
@@ -1,13 +1,37 @@
 import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
 
 function WelcomeComponent() {
     const { username } = useParams();
+
+    function callHelloWorldRestApi() {
+        console.log('called');
+
+        axios
+            .get('http://localhost:8080/hello-world')
+            .then((response) => successfulResponse(response))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log('cleanup'));
+    }
+
+    function successfulResponse(response) {
+        console.log(response);
+    }
+
+    function errorResponse(response) {
+        console.log(response);
+    }
 
     return (
         <div className="Welcome">
             <h1>Welcome {username}</h1>
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+                    Hello World Rest API 호출버튼
+                </button>
             </div>
         </div>
     );
