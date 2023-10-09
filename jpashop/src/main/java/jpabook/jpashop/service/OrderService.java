a6c2d0a6cf94dca239ebb4c596e848bf9e714a7e
beombu <beombu13@gmail.com>
2023-10-09 11:12:20 +0900

    Feat : 컨트롤러, 타임리프 개발

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -4,6 +4,7 @@ import jpabook.jpashop.domain.Delivery;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
@@ -45,5 +46,7 @@ public class OrderService {
         order.cancel();
     }
 
-    //public List<Order> find
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByCriteria(orderSearch);
+    }
 }
