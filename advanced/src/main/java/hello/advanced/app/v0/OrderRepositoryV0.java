993a1fc64fda9ca64d701568f12fdeb7bbd71823
beombu <beombu13@gmail.com>
2024-02-25 15:48:11 +0900

    Feat : 로그 추적기 - V0 & V1

---
+++ advanced/src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v0;
+
+import static java.lang.Thread.*;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+	public void save(String itemId) {
+
+		if (itemId.equals("ex")) {
+			throw new IllegalArgumentException("예외 발생!");
+		}
+		sleep(1000);
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
